c517e90696fcd5096704b95ea49c2b26938d5ce6
theGioiLa <theGioiLa@github.com>
2019-12-03 15:48:40 +0700

    [FIXED] mkdirp transcodePath before transcode

--- src/server/routes/upload.js
+++ src/server/routes/upload.js
@@ -19,9 +19,16 @@ router.post('/', async (req, res) => {
             file.pipe(dest)
 
             dest.on('finish', () => {
-                let dir = path.parse(dest.path).dir
+                let { base, dir } = path.parse(dest.path)
                 let transcodePath = path.join(dir, 'hls')
+                mkdirp.sync(transcodePath)
                 transcode(dest.path, transcodePath, () => {
+                    let key = base
+                    let info = {
+                        originalPath: dest.path,
+                        transcodePath
+                    }
+                    storage.set(key, JSON.stringify(info))
                 })
             })
         } catch (err) {
@@ -41,28 +48,28 @@ function transcode(src, dest, done) {
     console.log('Dest: ', dest)
 
     const command = ffmpeg(src)
-    .audioCodec('aac')
-    .audioBitrate(128)
-    .videoBitrate('1000')
-    // .outputOptions([
-    //     '-codec: copy',
-    //     '-hls_time 10',
-    //     '-hls_playlist_type vod',
-    //     '-hls_base_url http://localhost:8080/',
-    //     '-hls_segment_filename ~/%03d.ts'
-    // ])
-     .outputOptions([
-        '-hls_time 10'
-    ])
-    .output(path.join(dest, 'index.m3u8'))
-    .on('progress', function(progress) {
-        console.log('Processing: ' + progress.percent + '% done')
-    })
-    .on('end', function(err, stdout, stderr) {
-        done()
-        console.log('Finished processing!' /*, err, stdout, stderr*/)
-    })
-    .run()
+        .audioCodec('aac')
+        .audioBitrate(128)
+        .videoBitrate('1000')
+        // .outputOptions([
+        //     '-codec: copy',
+        //     '-hls_time 10',
+        //     '-hls_playlist_type vod',
+        //     '-hls_base_url http://localhost:8080/',
+        //     '-hls_segment_filename ~/%03d.ts'
+        // ])
+        .outputOptions([
+            '-hls_time 10'
+        ])
+        .output(path.join(dest, 'index.m3u8'))
+        .on('progress', function (progress) {
+            console.log('Processing: ' + progress.percent + '% done')
+        })
+        .on('end', function (err, stdout, stderr) {
+            done()
+            console.log('Finished processing!' /*, err, stdout, stderr*/)
+        })
+        .run()
 }
 
 module.exports = router
